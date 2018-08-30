a2d02262dd7811be9da448658caa771e78cbff0a
zzy96 <zhou0250@e.ntu.edu.sg>
2018-08-30 19:34:57 +0800

    preset gopath etc

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get install -y build-essential unzip libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-dev solc sysvbanner wrk haskell-platform
 #apt-get install -y zsh emacs25
 
-export HOME=/vagrant
+export VAGRANTHOME=/vagrant
 
 # install golang
 GOREL=go1.9.3.linux-amd64.tar.gz
@@ -16,7 +16,7 @@ tar xfz $GOREL
 mv go /usr/local/go
 rm -f $GOREL
 export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/go
+export GOPATH=$VAGRANTHOME/go
 export PATH=$PATH:$GOPATH/bin
 
 # (optional) install zsh
@@ -25,19 +25,20 @@ export PATH=$PATH:$GOPATH/bin
 # echo 'exec /bin/zsh -l' >> .bash_profile
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # curl -L git.io/antigen > antigen.zsh # install antigen
-# cp /vagrant/.zshrc $HOME/.zshrc
-# git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions # plugin
+# cp /vagrant/.zshrc $VAGRANTHOME/.zshrc
+# git clone https://github.com/zsh-users/zsh-autosuggestions $VAGRANTHOME/.zsh/zsh-autosuggestions # plugin
 
 # setting env var in zsh
-# echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.zshrc
-# echo 'export GOPATH=$HOME/go' >> $HOME/.zshrc
-# echo 'export PATH=$PATH:$GOPATH/bin' >> $HOME/.zshrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> $VAGRANTHOME/.bash_profile
+echo 'export GOPATH=/vagrant/go' >> $VAGRANTHOME/.bash_profile
+echo 'export PATH=$PATH:$GOPATH/bin' >> $VAGRANTHOME/.bash_profile
+echo 'cd /vagrant' >> $VAGRANTHOME/.bash_profile
 
 # clone repos
 git clone https://github.com/zzy96/quorum.git $GOPATH/src/github.com/ethereum/go-ethereum
 cd $GOPATH/src/github.com/ethereum/go-ethereum
 git remote add upstream https://github.com/jpmorganchase/quorum.git
-cd $HOME
+cd $VAGRANTHOME
 
 git clone https://github.com/jpmorganchase/constellation.git
 
@@ -49,15 +50,15 @@ git clone https://github.com/jpmorganchase/quorum-examples.git
 cd $GOPATH/src/github.com/ethereum/go-ethereum
 go install -v ./...
 
-chown -R vagrant:vagrant $HOME/quorum-tools $HOME/quorum-examples $HOME/constellation $GOPATH
+chown -R vagrant:vagrant $VAGRANTHOME/quorum-tools $VAGRANTHOME/quorum-examples $VAGRANTHOME/constellation $GOPATH
 
 # install haskell tools
 curl -sSL https://get.haskellstack.org/ | sh  # stack
 
 # (optional) install spacemacs
-# git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+# git clone https://github.com/syl20bnr/spacemacs $VAGRANTHOME/.emacs.d
 # git clone https://github.com/Szkered/dotfiles
-# ln -s dotfiles/.spacemacs $HOME/.spacemacs
+# ln -s dotfiles/.spacemacs $VAGRANTHOME/.spacemacs
 
 # (optional) install go-mode tools
 # go get -u -v github.com/nsf/gocode
@@ -67,6 +68,8 @@ curl -sSL https://get.haskellstack.org/ | sh  # stack
 # go get -u -v golang.org/x/tools/cmd/goimports
 # go get -u -v github.com/alecthomas/gometalinter
 # gometalinter --install --update
+
+source $VAGRANTHOME/.bash_profile
 
 # done!
 banner "Quorum Dev Box"
